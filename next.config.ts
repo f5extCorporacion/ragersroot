41e6f61e677f6857f56cc781abbb3d249eb9312d
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2026-06-13 16:27:45 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      // Si también usas GitHub o otras autenticaciones, puedes agregar:
+      // {
+      //   protocol: "https",
+      //   hostname: "avatars.githubusercontent.com",
+      //   port: "",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "platform-lookaside.fbsbx.com", // Para Facebook
+      //   port: "",
+      //   pathname: "/**",
+      // },
+    ],
+  },
 };
 
 export default nextConfig;
